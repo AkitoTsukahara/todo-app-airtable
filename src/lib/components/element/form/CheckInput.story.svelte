45e4a277c0cfda9dbc3d22fa3f321451770ced21
AkitoTsukahara <a-tsukahara@macloud.jp>
2023-01-07 23:54:51 +0900

    Add InputComponent

---
+++ src/lib/components/element/form/CheckInput.story.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import CheckInput from '$lib/components/element/form/CheckInput.svelte'
+
+  export let Hst
+  let isChecked = false
+</script>
+
+<Hst.Story>
+  <CheckInput
+      {isChecked}
+  >
+    Todo Item
+  </CheckInput>
+
+  <svelte:fragment slot="controls">
+    <Hst.Checkbox bind:value={isChecked} title="isChecked"/>
+    <pre>{JSON.stringify({ isChecked }, null, 2)}</pre>
+  </svelte:fragment>
+</Hst.Story>
