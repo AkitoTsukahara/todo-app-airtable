4ba7176f5b3a4e5f2fb31c818d5a9294f73e71c9
AkitoTsukahara <a-tsukahara@macloud.jp>
2023-01-09 23:55:14 +0900

    Add AbstractTime

---
+++ src/lib/components/element/AbstractTime.story.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import AbstractTime from '$lib/components/element/AbstractTime.svelte'
+
+  export let Hst
+</script>
+
+<Hst.Story>
+  <AbstractTime/>
+</Hst.Story>
---
+++ src/lib/components/element/AbstractTime.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  let time = '01月09日 19:00:00'
+  let displayTime = '19:00'
+</script>
+<a aria-label="{time}"
+   class="c-link c-timestamp"
+   href="#">
+  <span>{displayTime}</span>
+</a>
+
+<style lang="scss">
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+
+</style>
