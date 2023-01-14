43d50d95b1bf2ea0ec6a7c3007539fdcbbf46700
AkitoTsukahara <a-tsukahara@macloud.jp>
2023-01-14 22:57:42 +0900

    update AbstractTime Component

--- src/lib/components/element/AbstractTime.story.svelte
+++ src/lib/components/element/AbstractTime.story.svelte
@@ -2,8 +2,18 @@
   import AbstractTime from '$lib/components/element/AbstractTime.svelte'
 
   export let Hst
+  export let position
 </script>
 
 <Hst.Story>
   <AbstractTime/>
+
+  <svelte:fragment slot="controls">
+    <Hst.Select
+        bind:value={position}
+        options={['top' , 'right' , 'left' , 'bottom']}
+        title="Position"
+    />
+    <pre>{JSON.stringify({ position }, null, 2)}</pre>
+  </svelte:fragment>
 </Hst.Story>
