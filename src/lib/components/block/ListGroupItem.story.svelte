f82d0172ecee845c5a9f943664b75d66bab6b359
AkitoTsukahara <a-tsukahara@macloud.jp>
2023-01-15 23:05:17 +0900

    Add ListGroupItem

---
+++ src/lib/components/block/ListGroupItem.story.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import ListGroupItem from '$lib/components/block/ListGroupItem.svelte'
+
+  export let Hst
+  let isChecked = false
+  let item = 'Todo Item'
+</script>
+
+<Hst.Story>
+  <ListGroupItem
+      {isChecked}
+      {item}
+  />
+
+  <svelte:fragment slot="controls">
+    <Hst.Checkbox bind:value={isChecked} title="isChecked"/>
+    <Hst.Text bind:value={item} title="Item"/>
+    <pre>{JSON.stringify({ isChecked }, { item },null, 2)}</pre>
+  </svelte:fragment>
+</Hst.Story>
