88022a07f83f8314e23305b87c6eb46ba032f104
AkitoTsukahara <a-tsukahara@macloud.jp>
2023-02-26 00:35:11 +0900

    complete sample todo

--- src/routes/sample/+page.svelte
+++ src/routes/sample/+page.svelte
@@ -26,8 +26,8 @@
         </label>
       </div>
     </aside>
-    <main class="w-4/5 grid grid-cols-2 auto-rows-auto gap-6">
-      <div class="task gap-1 bg-amber-200 p-6 rounded-xl mb-6">
+    <main class="w-4/5 columns-2 gap-x-6">
+      <div class="task gap-1 bg-amber-200 p-6 rounded-xl mb-6 overflow-auto">
         <div class="task-top flex justify-between mb-4 items-center">
           <h2 class="text-2xl font-bold">The first task title</h2>
           <div>
@@ -64,7 +64,7 @@
           </label>
         </div>
       </div>
-      <div class="task gap-1 bg-amber-200 p-6 rounded-xl mb-6">
+      <div class="task gap-1 bg-amber-200 p-6 rounded-xl mb-6 overflow-auto">
         <div class="task-top flex justify-between mb-4 items-center">
           <h2 class="text-2xl font-bold">The second task title</h2>
           <div>
@@ -101,7 +101,7 @@
           </label>
         </div>
       </div>
-      <div class="task gap-1 bg-amber-200 p-6 rounded-xl mb-6">
+      <div class="task gap-1 bg-amber-200 p-6 rounded-xl mb-6 overflow-auto">
         <div class="task-top flex justify-between mb-4 items-center">
           <h2 class="text-2xl font-bold">The third task title</h2>
           <div>
@@ -138,7 +138,7 @@
           </label>
         </div>
       </div>
-      <div class="task gap-1 bg-amber-200 p-6 rounded-xl mb-6">
+      <div class="task gap-1 bg-amber-200 p-6 rounded-xl mb-6 overflow-auto">
         <div class="task-top flex justify-between mb-4 items-center">
           <h2 class="text-2xl font-bold">The fourth task title</h2>
           <div>
@@ -175,18 +175,80 @@
           </label>
         </div>
       </div>
+      <div class="task gap-1 bg-amber-200 p-6 rounded-xl mb-6 overflow-auto">
+        <div class="task-top flex justify-between mb-4 items-center">
+          <h2 class="text-2xl font-bold">The fifth task title</h2>
+          <div>
+            <button data-dropdown-toggle="dropdownDotsHorizontal" class="inline-flex items-center p-2 text-2xl font-medium text-center text-gray-900 rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none" type="button">
+              <svg class="w-8 h-8" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z"></path>
+              </svg>
+            </button>
+            <!-- Dropdown menu -->
+            <div class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44">
+              <ul class="py-2 text-sm text-gray-700" aria-labelledby="dropdownMenuIconHorizontalButton">
+                <li>
+                  <a href="#" class="block px-4 py-2 hover:bg-gray-100">Edit...</a>
+                </li>
+                <li>
+                  <a href="#" class="block px-4 py-2 hover:bg-gray-10">Delete</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <p class="text-xl mb-6">sample message.sample message.sample message.sample message.sample message.sample message.sample message.sample message.sample message.sample message.sample message.sample message.sample message.sample message.</p>
+        <div class="task-bottom flex justify-between">
+          <div class="tags">
+            <ul class="flex gap-2">
+              <li><a href="#work"><div class="w-8 h-8 bg-violet-300 rounded-full mr-2"></div></a></li>
+              <li><a href="#study"><div class="w-8 h-8 bg-blue-300 rounded-full mr-2"></div></a></li>
+              <li><a href="#entertainment"><div class="w-8 h-8 bg-red-300 rounded-full mr-2"></div></a></li>
+            </ul>
+          </div>
+          <label class="inline-flex items-center text-xl font-light">
+            <input type="checkbox" class="w-6 h-6 rounded-xl mr-2"/>
+            Done
+          </label>
+        </div>
+      </div>
+      <div class="task gap-1 bg-amber-200 p-6 rounded-xl mb-6 overflow-auto">
+        <div class="task-top flex justify-between mb-4 items-center">
+          <h2 class="text-2xl font-bold">The sixth task title</h2>
+          <div>
+            <button data-dropdown-toggle="dropdownDotsHorizontal" class="inline-flex items-center p-2 text-2xl font-medium text-center text-gray-900 rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none" type="button">
+              <svg class="w-8 h-8" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z"></path>
+              </svg>
+            </button>
+            <!-- Dropdown menu -->
+            <div class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44">
+              <ul class="py-2 text-sm text-gray-700" aria-labelledby="dropdownMenuIconHorizontalButton">
+                <li>
+                  <a href="#" class="block px-4 py-2 hover:bg-gray-100">Edit...</a>
+                </li>
+                <li>
+                  <a href="#" class="block px-4 py-2 hover:bg-gray-10">Delete</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <p class="text-xl mb-6">sample message.sample message.sample message.sample message.sample message.sample message.sample message.</p>
+        <div class="task-bottom flex justify-between">
+          <div class="tags">
+            <ul class="flex gap-2">
+              <li><a href="#work"><div class="w-8 h-8 bg-violet-300 rounded-full mr-2"></div></a></li>
+              <li><a href="#study"><div class="w-8 h-8 bg-blue-300 rounded-full mr-2"></div></a></li>
+              <li><a href="#entertainment"><div class="w-8 h-8 bg-red-300 rounded-full mr-2"></div></a></li>
+            </ul>
+          </div>
+          <label class="inline-flex items-center text-xl font-light">
+            <input type="checkbox" class="w-6 h-6 rounded-xl mr-2"/>
+            Done
+          </label>
+        </div>
+      </div>
     </main>
   </div>
-</div>
-
-<div class="grid grid-cols-3 grid-auto-rows-[200px,auto] gap-4">
-  <div class="bg-gray-200">要素1<br>
-    要素1<br>
-    要素1
-    要素1</div>
-  <div class="bg-gray-300">要素2</div>
-  <div class="bg-gray-400">要素3</div>
-  <div class="bg-gray-500">要素4</div>
-  <div class="bg-gray-600">要素5</div>
-  <div class="bg-gray-700">要素6</div>
 </div>
