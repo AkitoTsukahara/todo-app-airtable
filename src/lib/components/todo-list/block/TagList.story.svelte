a5cbc56b3a05e168170660fe0b06ebb965f35e3d
AkitoTsukahara <a-tsukahara@macloud.jp>
2023-02-28 09:39:13 +0900

    tag list component

---
+++ src/lib/components/todo-list/block/TagList.story.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import TagList from '$lib/components/todo-list/block/TagList.svelte'
+
+  export let Hst
+</script>
+
+<Hst.Story>
+  <TagList/>
+
+  <svelte:fragment slot="controls"></svelte:fragment>
+
+</Hst.Story>
---
+++ src/lib/components/todo-list/block/TagList.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  type Tag = {
+    key: string
+    color: string
+  }
+  const tagList: Tag[] = [
+    {key: 'work', color: 'bg-violet-300'},
+    {key: 'study', color: 'bg-blue-300'},
+    {key: 'entertainment', color: 'bg-red-300'},
+    {key: 'family', color: 'bg-green-300'},
+  ]
+</script>
+
+<ul class="mb-16">
+  {#each tagList as tag}
+    <li class="mb-6">
+      <a href="#{ tag.key }" class="flex items-center gap-2">
+        <div class="w-8 h-8 { tag.color } rounded-full mr-2"></div>
+        { tag.key }
+      </a>
+    </li>
+  {/each}
+</ul>
